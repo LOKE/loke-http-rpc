7ff0c0171a30151c7b9ca8b3014c1afe2351047c
Den Williams <dennis.williams@loke.com.au>
2018-11-26 13:23:44 +1100

    add prettier

--- index.js
+++ index.js
@@ -1,20 +1,32 @@
-const pTap = require('p-tap');
-const pFinally = require('p-finally')
-const {Histogram, Counter} = require('prom-client');
+const pTap = require("p-tap");
+const pFinally = require("p-finally");
+const { Histogram, Counter } = require("prom-client");
 
-const requestDuration = new Histogram('http_rpc_request_duration_seconds', 'Duration of rpc requests', ['handler']);
-const requestCount = new Counter('http_rpc_requests_total', 'The total number of rpc requests received', ['handler']);
-const failureCount = new Counter('http_rpc_failures_total', 'The total number of rpc failures received', ['handler']);
+const requestDuration = new Histogram(
+  "http_rpc_request_duration_seconds",
+  "Duration of rpc requests",
+  ["handler"]
+);
+const requestCount = new Counter(
+  "http_rpc_requests_total",
+  "The total number of rpc requests received",
+  ["handler"]
+);
+const failureCount = new Counter(
+  "http_rpc_failures_total",
+  "The total number of rpc failures received",
+  ["handler"]
+);
 
 class ExtendableError extends Error {
   constructor(message) {
     super(message);
     this.name = this.constructor.name;
     this.message = message;
-    if (typeof Error.captureStackTrace === 'function') {
+    if (typeof Error.captureStackTrace === "function") {
       Error.captureStackTrace(this, this.constructor);
     } else {
-      this.stack = (new Error(message)).stack;
+      this.stack = new Error(message).stack;
     }
   }
 }
@@ -40,46 +52,47 @@ exports.createRequestHandler = (service, serviceDetails) => {
         methodTimeout: 60000
       };
     })
-  }
+  };
 
   return (req, res, next) => {
     const methodName = req.path.slice(1); // remove leading slash
     const body = req.body;
     const args = multiArg ? body : [body];
 
-    if (req.method === 'GET' && req.path === '/') {
+    if (req.method === "GET" && req.path === "/") {
       return res.json(meta);
     }
 
-    if (req.method !== 'POST' || exposed.indexOf(methodName) === -1) {
+    if (req.method !== "POST" || exposed.indexOf(methodName) === -1) {
       return next();
     }
 
     if (!Array.isArray(args)) {
       return res.status(400).json({
-        message: 'multiArg services require an array as input',
-        code: 'CRIT_INPUT_ERR'
+        message: "multiArg services require an array as input",
+        code: "CRIT_INPUT_ERR"
       });
     }
 
-    const requestMeta = {handler: `${serviceName}.${methodName}`};
+    const requestMeta = { handler: `${serviceName}.${methodName}` };
     const end = requestDuration.startTimer(requestMeta);
 
     requestCount.inc(requestMeta);
 
     const result = Promise.resolve()
-    .then(() => service[methodName].apply(service, args))
-    .then(result => res.json(result))
-    .catch(pTap.catch(() => failureCount.inc(requestMeta)))
-    .catch(err => {
-      next(new RpcError(methodName, err));
-    });
+      .then(() => service[methodName].apply(service, args))
+      .then(result => res.json(result))
+      .catch(pTap.catch(() => failureCount.inc(requestMeta)))
+      .catch(err => {
+        next(new RpcError(methodName, err));
+      });
 
     return pFinally(result, end);
-  }
+  };
 };
 
 exports.createErrorHandler = () => {
+  // eslint-disable-next-line no-unused-vars
   return (err, req, res, next) => {
     if (err instanceof RpcError) {
       res.status(400).json({
